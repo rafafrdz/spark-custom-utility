02b293f276e6a50e227a9b362da0c3c2e972efa0
Rafael Fernández <40717893+rafafrdz@users.noreply.github.com>
2021-08-27 14:08:40 +0200

    add mv and move method

--- src/main/scala/org/malaka/dev/io/FSystem.scala
+++ src/main/scala/org/malaka/dev/io/FSystem.scala
@@ -3,23 +3,48 @@ package org.malaka.dev.io
 import org.malaka.dev.core.session.Sparkable
 import org.malaka.dev.io.FStatus.{getChild, status, traverseStatuses}
 
+import scala.util.{Failure, Success, Try}
+
 object FSystem extends HadoopFileSystem with Sparkable {
+
+
+  def rm(path: String, recursive: Boolean = false): Unit = delete(path, recursive)
 
   def delete(path: String, recursive: Boolean = false): Unit = fs.delete(hdfs(path), recursive)
 
+  def mv(path: String, otherPath: String): Boolean = mover(path, otherPath)
+
+  def mover(path: String, otherPath: String): Boolean = {
+    val isFile: Boolean = !isDirectory(path)
+    lazy val move: Boolean = fs.rename(hdfs(path), hdfs(otherPath))
+    if (isFile) Try {
+      delete(otherPath)
+      move
+      delete(path)
+      move
+    } match {
+      case Success(value) => value
+      case Failure(exception) =>
+        logger.error(exception.getMessage)
+        false
+    }
+    else isFile
+  }
+
   def mkdirs(path: String, overwrite: Boolean = false): Unit = {
-    if(overwrite) delete(path, recursive = true)
+    if (overwrite) delete(path, recursive = true)
     fs.mkdirs(hdfs(path))
   }
 
   def exist(path: String): Boolean = fs.exists(hdfs(path))
+
   def isDirectory(path: String): Boolean = fs.isDirectory(hdfs(path))
 
   def lsTree(root: String, onlyFiles: Boolean = true): Array[String] = {
     status(root) match {
       case Some(statuses) =>
         lazy val paths = traverseStatuses(statuses, onlyFiles)
-        if(onlyFiles) paths else root +: paths
+        if (onlyFiles) paths else root +: paths
       case None => Array.empty[String]
     }
   }
